965ef449584a8fbd53451eaba631c1182c177c58
TomNotArtem <ablazhievskii@gmail.com>
2023-03-14 20:02:02 +0800

    Removed unnecessary import and changed in the manifest path to the activity

--- app/src/main/java/ru/easycode/words504/MainActivity.kt
+++ app/src/main/java/ru/easycode/words504/MainActivity.kt
@@ -2,7 +2,6 @@ package ru.easycode.words504
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import ru.easycode.words504.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
