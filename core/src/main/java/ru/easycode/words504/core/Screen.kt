5c1742d2490fea66f5c7e70bba3801ab69f3c5f4
Nilsolk <nilsolk.bat@gmail.com>
2023-03-15 19:13:01 +0300

    CORE-0020 navigation

--- core/src/main/java/ru/easycode/words504/core/Screen.kt
+++ core/src/main/java/ru/easycode/words504/core/Screen.kt
@@ -4,14 +4,15 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 
 interface Screen {
-
     fun replace(manager: FragmentManager)
 
-    abstract class Base(private val containerId: Int, private val fragment: Fragment) : Screen {
+    abstract class Base(private val clazz: Class<Fragment>) : Screen {
 
+        protected abstract val resId: Int
         override fun replace(manager: FragmentManager) {
-            manager.beginTransaction().replace(containerId, fragment).commit()
+            manager.beginTransaction().replace(resId, clazz.newInstance())
+                .commit()
         }
-
     }
 }
+
