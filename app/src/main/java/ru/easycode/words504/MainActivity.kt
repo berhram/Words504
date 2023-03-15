fb262864f4ce81d07cbcefd20357d17005a38227
blastka <svetuk505@mail.ru>
2023-03-15 13:45:17 +0300

    Added code to launch test activity from main activity

--- app/src/main/java/ru/easycode/words504/MainActivity.kt
+++ app/src/main/java/ru/easycode/words504/MainActivity.kt
@@ -1,11 +1,19 @@
 package ru.easycode.words504
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import ru.easycode.words504.recognition.TestVoiceRecognitionActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val testButton = findViewById<Button>(R.id.testButton)
+        testButton.setOnClickListener {
+            val intent = Intent(this, TestVoiceRecognitionActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/ru/easycode/words504/recognition/TestVoiceRecognitionActivity.kt
@@ -0,0 +1,12 @@
+package ru.easycode.words504.recognition
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.easycode.words504.R
+
+class TestVoiceRecognitionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test_voice_recognition)
+    }
+}
