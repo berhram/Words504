f80698a13c477aec7a6eb70e4c56a668083ebbf4
EkZakh <ekzakh14@gmail.com>
2023-03-16 12:42:18 +0300

    fix fun name

--- core/src/main/java/ru/easycode/words504/core/sl/ProvideViewModel.kt
+++ core/src/main/java/ru/easycode/words504/core/sl/ProvideViewModel.kt
@@ -4,5 +4,5 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelStoreOwner
 
 interface ProvideViewModel {
-    fun <T : ViewModel> provide(clazz: Class<T>, owner: ViewModelStoreOwner): T
+    fun <T : ViewModel> viewModel(clazz: Class<T>, owner: ViewModelStoreOwner): T
 }
