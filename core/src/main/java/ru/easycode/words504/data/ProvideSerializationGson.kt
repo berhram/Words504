1d4acab33889042ddd1fb9178c313d53c0446f12
Sabina Zishko <2781157az>
2023-03-16 15:46:53 +0300

    CORE-0011 implementation

--- core/src/main/java/ru/easycode/words504/data/ProvideSerializationGson.kt
+++ core/src/main/java/ru/easycode/words504/data/ProvideSerializationGson.kt
@@ -1,18 +1,22 @@
 package ru.easycode.words504.data
 
+import com.google.gson.Gson
+import java.util.Objects
+
 interface ProvideSerializationGson {
 
     fun toJson(str: Any): String
 
-    fun <T>toString(json:String, obj: T): T
+    fun <T>fromJson(json:String,obj: Class<T>): T
 
-    class Base(): ProvideSerializationGson {
+    class Base(private val gson: Gson = Gson()): ProvideSerializationGson {
         override fun toJson(str: Any): String {
-            TODO("Not yet implemented")
+            return gson.toJson(str)
         }
 
-        override fun <T> toString(json: String, obj: T): T {
-            TODO("Not yet implemented")
+        override fun <T> fromJson(json: String,obj: Class<T>): T {
+          return  gson.fromJson(json,obj)
         }
     }
 }
+
