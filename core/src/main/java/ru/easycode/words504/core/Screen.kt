2af4d7277c4ac2a61d0cacbd3ddda0b9fdbcd565
Nilsolk <nilsolk.bat@gmail.com>
2023-03-16 16:00:15 +0300

    CORE-0020 inheritors of Fragment

--- core/src/main/java/ru/easycode/words504/core/Screen.kt
+++ core/src/main/java/ru/easycode/words504/core/Screen.kt
@@ -7,7 +7,7 @@ interface Screen {
     fun replace(manager: FragmentManager)
 
     abstract class Base(
-        private val clazz: Class<Fragment>,
+        private val clazz: Class<out Fragment>,
         private val resId: Int = com.google.android.material.R.id.container
     ) : Screen {
         override fun replace(manager: FragmentManager) {
