c24ce84064311e43318e4833397c2284bb44a0d4
Sabina Zishko <you@example.com>
2023-03-17 19:19:00 +0300

    CLOUD-0003 rename abstract class, create base class

--- core/src/main/java/ru/easycode/words504/data/ProvideRetrofitBuilder.kt
+++ core/src/main/java/ru/easycode/words504/data/ProvideRetrofitBuilder.kt
@@ -6,10 +6,16 @@ interface ProvideRetrofitBuilder {
 
     fun provideRetrofitBuilder(): Retrofit.Builder
 
-    class Base(
+    abstract class Abstract(
         private val httpClientBuilder: ProvideOkHttpClientBuilder
     ) : ProvideRetrofitBuilder {
         override fun provideRetrofitBuilder(): Retrofit.Builder = Retrofit.Builder()
             .client(httpClientBuilder.httpClientBuilder().build())
     }
+
+    class Base(
+        httpClientBuilder: ProvideOkHttpClientBuilder,
+    ) : Abstract(
+        httpClientBuilder
+    )
 }
