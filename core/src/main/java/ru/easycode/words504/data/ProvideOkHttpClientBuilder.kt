819d1a69c9b1a9946f01948109bade0e8f2a8206
AntonEsipow <esipow.anton99@gmail.com>
2023-03-17 15:18:01 +0300

    CLOUD-0002 timeOutProperty

--- core/src/main/java/ru/easycode/words504/data/ProvideOkHttpClientBuilder.kt
+++ core/src/main/java/ru/easycode/words504/data/ProvideOkHttpClientBuilder.kt
@@ -9,15 +9,14 @@ interface ProvideOkHttpClientBuilder {
 
     abstract class Abstract(
         private val provideInterceptor: ProvideInterceptor,
+        private val timeOutProperty: TimeUnit = TimeUnit.SECONDS,
         private val timeOutSeconds: Long = 60L
     ): ProvideOkHttpClientBuilder {
 
-        private val timeUnit: TimeUnit = TimeUnit.SECONDS
-
         override fun httpClientBuilder() = OkHttpClient.Builder()
             .addInterceptor(provideInterceptor.interceptor())
-            .connectTimeout(timeOutSeconds, timeUnit)
-            .readTimeout(timeOutSeconds, timeUnit)
+            .connectTimeout(timeOutSeconds, timeOutProperty)
+            .readTimeout(timeOutSeconds, timeOutProperty)
     }
 
     class Base(
