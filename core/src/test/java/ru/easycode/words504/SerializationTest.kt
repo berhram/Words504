8ddb1f1119eabad21d8dc10946de9e5d4022a508
Sabina Zishko <you@example.com>
2023-03-17 08:46:57 +0300

    CORE-0011 fix Serialization.kt

--- core/src/test/java/ru/easycode/words504/SerializationTest.kt
+++ core/src/test/java/ru/easycode/words504/SerializationTest.kt
@@ -4,19 +4,18 @@ import com.google.gson.Gson
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotEquals
 import org.junit.Test
-import ru.easycode.words504.data.ProvideSerialization
+import ru.easycode.words504.data.Serialization
 
 
-class ProvideSerializationTest() {
-
-    data class FakeSerializedObject(val id: String)
+class SerializationTest() {
+    private data class FakeSerializedObject(val id: String)
 
     private val gson = Gson()
-    private val provideSerialization = ProvideSerialization.Base(gson)
+    private val serialization = Serialization.Base(gson)
 
     @Test
     fun readEmptyString() {
-        val actual = provideSerialization.toJson(FakeSerializedObject(""))
+        val actual = serialization.toJson(FakeSerializedObject(""))
         val expected = "{\"id\":\"\"}"
         assertEquals(expected, actual)
     }
@@ -24,7 +23,7 @@ class ProvideSerializationTest() {
     @Test
     fun readEmptyObject() {
         val actual =
-            provideSerialization.fromJson("{\"id\":\"\"}", FakeSerializedObject::class.java)
+            serialization.fromJson("{\"id\":\"\"}", FakeSerializedObject::class.java)
         val expected = FakeSerializedObject("")
         assertEquals(expected, actual)
     }
@@ -32,14 +31,15 @@ class ProvideSerializationTest() {
     @Test
     fun readRightObject() {
         val actual =
-            provideSerialization.fromJson("{\"id\":\"777\"}", FakeSerializedObject::class.java)
+            serialization.fromJson("{\"id\":\"777\"}", FakeSerializedObject::class.java)
         val expected = "{\"id\":\"777\"}"
         assertNotEquals(actual, expected)
     }
+
     @Test
     fun readRightString() {
         val actual =
-            provideSerialization.toJson(FakeSerializedObject("id:777"))
+            serialization.toJson(FakeSerializedObject("id:777"))
         val expected = "{\"id\":\"777\"}"
         assertNotEquals(actual, expected)
     }
