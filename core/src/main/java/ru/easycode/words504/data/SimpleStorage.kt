5ef9a6238fe26eac28957e0b3e4e00420d5ea62f
banancheg <artemmakuwev@gmail.com>
2023-03-17 12:40:56 +0500

    CORE-0013 fix !!

--- core/src/main/java/ru/easycode/words504/data/SimpleStorage.kt
+++ core/src/main/java/ru/easycode/words504/data/SimpleStorage.kt
@@ -4,9 +4,8 @@ interface SimpleStorage : StringStorage {
     class Base(provideSharedPref: ProvideSharedPreferences) : SimpleStorage {
 
         private val sharedPreferences = provideSharedPref.sharedPreferences()
-        override fun read(key: String, default: String): String {
-            return sharedPreferences.getString(key, default)!!
-        }
+        override fun read(key: String, default: String) =
+            sharedPreferences.getString(key, default) ?: default
 
         override fun save(key: String, value: String) {
             sharedPreferences.edit().putString(key, value).apply()
