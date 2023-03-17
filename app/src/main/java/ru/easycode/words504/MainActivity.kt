49ca4917ffd6a52cd01d61019b34bcc041aef480
velvet <berhram@protonmail.com>
2023-03-17 18:29:55 +0300

    feature/ci-0002_lint_on_pr

--- app/src/main/java/ru/easycode/words504/MainActivity.kt
+++ app/src/main/java/ru/easycode/words504/MainActivity.kt
@@ -2,10 +2,26 @@ package ru.easycode.words504
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.easycode.words504.R
+import ru.easycode.words504.databinding.ActivityMainBinding
+
+
+
+
+
+
 
 class MainActivity : AppCompatActivity() {
+
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+
+
+
     setContentView(R.layout.activity_main)
     }
 }
