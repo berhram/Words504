b513ebe8325a67877fd990a66226d29a662acd3c
VnukovNS <95089447+VnukovNS@users.noreply.github.com>
2023-03-17 22:52:08 +0300

    CLOUD-0003 Add Base class again

--- core/src/main/java/ru/easycode/words504/data/ProvideRetrofitBuilder.kt
+++ core/src/main/java/ru/easycode/words504/data/ProvideRetrofitBuilder.kt
@@ -1,21 +1,25 @@
 package ru.easycode.words504.data
 
 import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 interface ProvideRetrofitBuilder {
 
     fun provideRetrofitBuilder(): Retrofit.Builder
 
-    abstract class Base (
+    abstract class Abstract(
         private val provideConverterFactory: ProvideConverterFactory,
         private val httpClientBuilder: ProvideOkHttpClientBuilder
     ) : ProvideRetrofitBuilder {
         override fun provideRetrofitBuilder(): Retrofit.Builder = Retrofit.Builder()
-            .client(httpClientBuilder.httpClientBuilder().build())
-            .baseUrl(baseUrl)
             .addConverterFactory(provideConverterFactory.converterFactory())
-
-        private val baseUrl: String = "testUrl"
+            .client(httpClientBuilder.httpClientBuilder().build())
     }
+
+    class Base(
+        provideConverterFactory: ProvideConverterFactory,
+        httpClientBuilder: ProvideOkHttpClientBuilder
+    ) : Abstract(
+        provideConverterFactory,
+        httpClientBuilder
+    )
 }
