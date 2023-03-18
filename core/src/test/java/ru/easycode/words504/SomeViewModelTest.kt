d0c407fac664004afb95cc3236b42a303a0274f0
Stas Zareckii <zareckii@gmail.com>
2023-03-18 14:47:53 +0300

    FIX-0001 SomeViewModelTest

--- core/src/test/java/ru/easycode/words504/SomeViewModelTest.kt
+++ core/src/test/java/ru/easycode/words504/SomeViewModelTest.kt
@@ -1,20 +1,18 @@
 package ru.easycode.words504
 
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import ru.easycode.words504.core.presentation.BaseViewModel
 import ru.easycode.words504.presentation.DispatchersList
 
 class SomeViewModelTest : BaseTest() {
 
-    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
-    fun `test translate`() = runTest {
+    fun `test translate`() = runBlocking {
         val dispatchers = TestDispatchersList()
         val viewModel = SomeViewModel(dispatchers)
         viewModel.translate("example")
@@ -23,7 +21,8 @@ class SomeViewModelTest : BaseTest() {
 
     private class SomeViewModel(
         private val dispatchers: DispatchersList,
-    ) : ViewModel() {
+    ) : BaseViewModel(dispatchers) {
+
         var result: String = ""
 
         fun translate(word: String) {
