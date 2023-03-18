656e6940455bd368b30012f51b3cd2190f241492
nick <n1ck.bl4ck0ut@gmail.com>
2023-03-18 22:34:45 +0300

    TTS-0003 fix MediaLevel

--- core/src/main/java/ru/easycode/words504/tts/MediaLevel.kt
+++ core/src/main/java/ru/easycode/words504/tts/MediaLevel.kt
@@ -4,21 +4,14 @@ import android.media.AudioManager
 
 interface MediaLevel {
 
-    fun currentLevel(): Int
-
-    fun maxLevel(): Int
-
-    fun isLowLevel(lowLevelPercentage: Int = 30): Boolean
+    fun isLowLevel(): Boolean
 
     class Base(
         private val manager: AudioManager,
-        private val streamType: Int = AudioManager.STREAM_MUSIC,
+        private val streamType: Int = AudioManager.STREAM_MUSIC
     ) : MediaLevel {
-        override fun currentLevel() = manager.getStreamVolume(streamType)
 
-        override fun maxLevel() = manager.getStreamMaxVolume(streamType)
-
-        override fun isLowLevel(lowLevelPercentage: Int) =
-            lowLevelPercentage >= 100 * currentLevel() / maxLevel()
+        override fun isLowLevel() = 30 >= 100 * manager.getStreamVolume(streamType) /
+                manager.getStreamMaxVolume(streamType)
     }
 }
