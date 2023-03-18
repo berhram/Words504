b633b59f409b6eb7020e877d59e72a2a09fb35de
Roman Troyan <troyan.roman1985@gmail.com>
2023-03-18 13:30:59 +0300

    CLOUD-0004 fixed constructor &  heir added

--- core/src/main/java/ru/easycode/words504/core/data/MakeService.kt
+++ core/src/main/java/ru/easycode/words504/core/data/MakeService.kt
@@ -7,17 +7,19 @@ interface MakeService {
     fun <T : Any> service(clazz: Class<T>): T
 
     abstract class Abstract(
-        private val retrofitBuilder: ProvideRetrofitBuilder
+        private val retrofitBuilder: ProvideRetrofitBuilder,
+        private val baseUrl: String
     ) : MakeService {
 
         private val retrofit by lazy {
             retrofitBuilder.provideRetrofitBuilder()
-                .baseUrl(baseUrl())
+                .baseUrl(baseUrl)
                 .build()
         }
 
         override fun <T : Any> service(clazz: Class<T>): T = retrofit.create(clazz)
-
-        protected open fun baseUrl(): String = "https://api-free.deepl.com/v2/"
     }
+
+    class Dictionary(retrofitBuilder: ProvideRetrofitBuilder) :
+        Abstract(retrofitBuilder, "https://api-free.deepl.com/v2/")
 }
