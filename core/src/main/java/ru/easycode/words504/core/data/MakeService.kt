403a4b7a43aea0612ef250e72ad557f3dae5610c
Roman Troyan <troyan.roman1985@gmail.com>
2023-03-18 13:00:20 +0300

    CLOUD-0004 service interface name fixed

---
+++ core/src/main/java/ru/easycode/words504/core/data/MakeService.kt
@@ -0,0 +1,23 @@
+package ru.easycode.words504.core.data
+
+import ru.easycode.words504.data.ProvideRetrofitBuilder
+
+interface MakeService {
+
+    fun <T> service(clazz: Class<T>): T
+
+    abstract class Abstract(
+        private val retrofitBuilder: ProvideRetrofitBuilder,
+    ) : MakeService {
+
+        private val retrofit by lazy {
+            retrofitBuilder.provideRetrofitBuilder()
+                .baseUrl(baseUrl())
+                .build()
+        }
+
+        override fun <T> service(clazz: Class<T>): T = retrofit.create(clazz)
+
+        protected open fun baseUrl(): String = "https://api-free.deepl.com/v2/"
+    }
+}
