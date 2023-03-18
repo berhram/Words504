d27c5c95b0071ca66d1f321713f6796e36688581
TroyanRoman <61114665+TroyanRoman@users.noreply.github.com>
2023-03-18 14:05:23 +0300

    CLOUD-0004 service interface space added (#36)
    * CLOUD-0004 service interface

---
+++ core/src/main/java/ru/easycode/words504/core/data/MakeService.kt
@@ -0,0 +1,25 @@
+package ru.easycode.words504.core.data
+
+import ru.easycode.words504.data.ProvideRetrofitBuilder
+
+interface MakeService {
+
+    fun <T : Any> service(clazz: Class<T>): T
+
+    abstract class Abstract(
+        private val retrofitBuilder: ProvideRetrofitBuilder,
+        private val baseUrl: String
+    ) : MakeService {
+
+        private val retrofit by lazy {
+            retrofitBuilder.provideRetrofitBuilder()
+                .baseUrl(baseUrl)
+                .build()
+        }
+
+        override fun <T : Any> service(clazz: Class<T>): T = retrofit.create(clazz)
+    }
+
+    class Dictionary(retrofitBuilder: ProvideRetrofitBuilder) :
+        Abstract(retrofitBuilder, "https://api-free.deepl.com/v2/")
+}
