9c868a68d3237a2998d253d5e7c3f99a0f540b10
EkZakh <ekzakh14@gmail.com>
2023-03-18 09:09:16 +0300

    fix

--- core/src/main/java/ru/easycode/words504/data/ObjectStorage.kt
+++ core/src/main/java/ru/easycode/words504/data/ObjectStorage.kt
@@ -4,7 +4,7 @@ interface ObjectStorage {
 
     fun save(key: String, obj: Any)
 
-    fun <T> read(key: String, default: Any, clazz: Class<T>): T
+    fun <T : Any> read(key: String, default: T, clazz: Class<T>): T
 
     class Base(
         private val serialization: Serialization,
@@ -16,10 +16,10 @@ interface ObjectStorage {
             stringStorage.save(key, json)
         }
 
-        override fun <T> read(key: String, default: Any, clazz: Class<T>): T {
-            val defaultJson = serialization.toJson(default)
+        override fun <T : Any> read(key: String, default: T, clazz: Class<T>): T {
+            val defaultJson = serialization.toJson(default as Any)
             val json = stringStorage.read(key, defaultJson)
-            return serialization.fromJson(json, clazz)
+            return serialization.fromJson(json, default::class.java)
         }
     }
 }
