4755d3343e136acc58dc19243ab3ac97bdbcd2f2
Sabina Zishko <you@example.com>
2023-03-21 20:54:42 +0300

    tts-0002 delete extra interfaces

--- core/src/main/java/ru/easycode/words504/tts/TTSCommunication.kt
+++ core/src/main/java/ru/easycode/words504/tts/TTSCommunication.kt
@@ -3,12 +3,5 @@ package ru.easycode.words504.tts
 import ru.easycode.words504.presentation.Communication
 
 interface TTSCommunication {
-
-    interface Update : Communication.Update<List<String>>
-
-    interface Observe : Communication.Observe<List<String>>
-
-    interface Mutable : Update, Observe
-
-    class Base : Communication.Abstract<List<String>>(), Mutable
+    class Base : Communication.Abstract<List<String>>()
 }
