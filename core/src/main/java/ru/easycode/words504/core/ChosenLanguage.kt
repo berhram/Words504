34714130fbfb5a5106af73ea52a97bf62b0b4e52
Nilsolk <nilsolk.bat@gmail.com>
2023-03-21 17:07:40 +0300

    DICT-0003 Redone

--- core/src/main/java/ru/easycode/words504/core/ChosenLanguage.kt
+++ core/src/main/java/ru/easycode/words504/core/ChosenLanguage.kt
@@ -2,11 +2,24 @@ package ru.easycode.words504.core
 
 import ru.easycode.words504.data.ObjectStorage
 
-interface ChosenLanguage : ObjectStorage {
-
-    class Base(private val objectStorage: ObjectStorage) : ChosenLanguage {
-        override fun save(key: String, obj: Any) = objectStorage.save(key, obj)
-
-        override fun <T : Any> read(key: String, default: T): T = objectStorage.read(key, default)
+interface ChosenLanguage {
+    interface Read {
+        fun <T : Any> read(default: T): T
     }
+
+    interface Save {
+        fun save(obj: Any)
+    }
+
+    interface Mutable : Read, Save
+
+    class Base(private val objectStorage: ObjectStorage) : Mutable {
+        override fun <T : Any> read(default: T): T = objectStorage.read(LANGUAGE_KEY, default)
+        override fun save(obj: Any) = objectStorage.save(LANGUAGE_KEY, obj)
+
+        companion object {
+            private const val LANGUAGE_KEY = "ChosenLanguageKey"
+        }
+    }
+
 }
---
+++ core/src/main/java/ru/easycode/words504/core/ChosenLanguageCache.kt
@@ -0,0 +1,9 @@
+package ru.easycode.words504.core
+
+interface LanguageCache : Empty {
+
+    class Base(private val key: String, private val name: String) : Empty {
+        override fun isEmpty() = key.isEmpty() || name.isEmpty()
+
+    }
+}
