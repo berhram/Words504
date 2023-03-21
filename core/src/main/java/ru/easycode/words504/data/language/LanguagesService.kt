75372105b79fc44d48b45cb4cbb8923177b1f492
Yurii Surzhykov <yuriisurzhykov@gmail.com>
2023-03-21 02:06:03 +0200

    DICT-0002 - fixed response value for list of languages

--- core/src/main/java/ru/easycode/words504/data/language/LanguagesService.kt
+++ core/src/main/java/ru/easycode/words504/data/language/LanguagesService.kt
@@ -1,12 +1,11 @@
 package ru.easycode.words504.data.language
 
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface LanguagesService {
 
     @GET("languages")
-    suspend fun getLanguages(
-        @Query("type") type: String
-    ): LanguageCloud
+    suspend fun getLanguages(@Query("type") type: String): Response<List<LanguageCloud>>
 }
