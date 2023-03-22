63c51c4c38b654b6c55431999ec9ab8a8b6b6176
blastka <svetuk505@mail.ru>
2023-03-22 15:20:41 +0300

    VRG-0001 added speechRecognizer in  test activity

--- app/src/main/java/ru/easycode/words504/recognition/TestVoiceRecognitionActivity.kt
+++ app/src/main/java/ru/easycode/words504/recognition/TestVoiceRecognitionActivity.kt
@@ -1,12 +1,114 @@
 package ru.easycode.words504.recognition
 
-import androidx.appcompat.app.AppCompatActivity
+
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.speech.RecognitionListener
+import android.speech.RecognizerIntent
+import android.speech.SpeechRecognizer
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import ru.easycode.words504.R
+import java.util.*
 
 class TestVoiceRecognitionActivity : AppCompatActivity() {
+
+    lateinit var speechRecognizer: SpeechRecognizer
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                // Permission is granted. Continue the action or workflow in your
+                // app.
+            } else {
+                // Explain to the user that the feature is unavailable because the
+                // feature requires a permission that the user has denied. At the
+                // same time, respect the user's decision. Don't link to system
+                // settings in an effort to convince the user to change their
+                // decision.
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test_voice_recognition)
+
+        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this)
+
+        val speechRecognizerIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+        speechRecognizerIntent.putExtra(
+            RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+            RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+        )
+
+        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.ENGLISH.language)
+
+        val textView = findViewById<TextView>(R.id.resultTextView)
+
+        speechRecognizer.setRecognitionListener(object : RecognitionListener {
+            override fun onReadyForSpeech(p0: Bundle?) {
+                textView.text = resources.getString(R.string.speak)
+            }
+
+            override fun onBeginningOfSpeech() {
+                textView.text = resources.getString(R.string.listening)
+            }
+
+            override fun onRmsChanged(p0: Float) {
+
+            }
+
+            override fun onBufferReceived(p0: ByteArray?) {
+
+            }
+
+            override fun onEndOfSpeech() {
+
+            }
+
+            override fun onError(p0: Int) {
+                textView.text = resources.getString(R.string.result)
+            }
+
+            override fun onResults(p0: Bundle?) {
+                val data: ArrayList<String> =
+                    p0?.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION) as ArrayList<String>
+                textView.text = data[0]
+            }
+
+            override fun onPartialResults(p0: Bundle?) {
+
+            }
+
+            override fun onEvent(p0: Int, p1: Bundle?) {
+
+            }
+        })
+        val button = findViewById<Button>(R.id.speakButton)
+        button.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (ContextCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.RECORD_AUDIO
+                    ) == PackageManager.PERMISSION_GRANTED
+                ) {
+                    speechRecognizer.startListening(speechRecognizerIntent)
+                } else {
+                    requestPermissionLauncher.launch(
+                        Manifest.permission.RECORD_AUDIO
+                    )
+                }
+            }
+        }
     }
+
 }
