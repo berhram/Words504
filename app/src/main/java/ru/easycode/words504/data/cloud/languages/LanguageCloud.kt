6e5a72e985265960809bd593a56aa77858f191ac
lancerita <110335651+lancerita@users.noreply.github.com>
2023-03-23 08:48:45 +0300

    DICT-002-API-languages (#44)
    * LanguagesCloud and LanguagesService (API)

---
+++ app/src/main/java/ru/easycode/words504/data/cloud/languages/LanguageCloud.kt
@@ -0,0 +1,20 @@
+package ru.easycode.words504.data.cloud.languages
+
+import com.google.gson.annotations.SerializedName
+
+interface LanguageCloud {
+
+    fun isLanguageEnglish(): Boolean
+
+    data class Base(
+        @SerializedName("language")
+        private val languageCode: String,
+        @SerializedName("name")
+        private val name: String
+    ) : LanguageCloud {
+
+        override fun isLanguageEnglish(): Boolean {
+            return languageCode.lowercase().startsWith("en")
+        }
+    }
+}
