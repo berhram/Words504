45885d28cf62353a4acad63776f9c04243652a1e
velvet <berhram@protonmail.com>
2023-03-23 11:29:45 +0300

    CI-0003 lint rules

---
+++ rules/src/main/java/ru/easycode/rules/EasyCodeRuleSetProvider.kt
@@ -0,0 +1,11 @@
+package ru.easycode.rules
+
+import com.pinterest.ktlint.core.RuleProvider
+import com.pinterest.ktlint.core.RuleSetProviderV2
+
+class EasyCodeRuleSetProvider : RuleSetProviderV2("oop rules", NO_ABOUT) {
+
+    override fun getRuleProviders(): Set<RuleProvider> = setOf(
+        RuleProvider { EncapsulationRule() }
+    )
+}
