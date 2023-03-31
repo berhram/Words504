93c613cae41b4de2901ca0b016b92d968ae3820e
blastka <svetuk505@mail.ru>
2023-03-31 16:45:35 +0300

    feature/VRG-0001 voice recognizer engine

--- app/src/main/java/ru/easycode/words504/recognition/TestVoiceRecognitionActivity.kt
+++ app/src/main/java/ru/easycode/words504/recognition/TestVoiceRecognitionActivity.kt
@@ -2,6 +2,7 @@ package ru.easycode.words504.recognition
 
 
 import android.Manifest
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
@@ -20,79 +21,28 @@ import java.util.*
 
 class TestVoiceRecognitionActivity : AppCompatActivity() {
 
-    lateinit var speechRecognizer: SpeechRecognizer
-
-    private val requestPermissionLauncher =
-        registerForActivityResult(
-            ActivityResultContracts.RequestPermission()
-        ) { isGranted: Boolean ->
-            if (isGranted) {
-                // Permission is granted. Continue the action or workflow in your
-                // app.
-            } else {
-                // Explain to the user that the feature is unavailable because the
-                // feature requires a permission that the user has denied. At the
-                // same time, respect the user's decision. Don't link to system
-                // settings in an effort to convince the user to change their
-                // decision.
-            }
-        }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test_voice_recognition)
 
-        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this)
-
-        val speechRecognizerIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
-        speechRecognizerIntent.putExtra(
-            RecognizerIntent.EXTRA_LANGUAGE_MODEL,
-            RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
-        )
-
-        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.ENGLISH.language)
+        val speechRecognizerEngine = SpeechRecognizerEngine.Base(applicationContext)
 
         val textView = findViewById<TextView>(R.id.resultTextView)
 
-        speechRecognizer.setRecognitionListener(object : RecognitionListener {
-            override fun onReadyForSpeech(p0: Bundle?) {
-                textView.text = resources.getString(R.string.speak)
+        val callback = object : SpeechRecognizerCallback {
+            override fun started() {
+                textView.text = "Started"
             }
 
-            override fun onBeginningOfSpeech() {
-                textView.text = resources.getString(R.string.listening)
+            override fun finished(result: String) {
+                textView.text = result
             }
 
-            override fun onRmsChanged(p0: Float) {
-
+            override fun error(code: Int) {
+                textView.text = code.toString()
             }
+        }
 
-            override fun onBufferReceived(p0: ByteArray?) {
-
-            }
-
-            override fun onEndOfSpeech() {
-
-            }
-
-            override fun onError(p0: Int) {
-                textView.text = resources.getString(R.string.result)
-            }
-
-            override fun onResults(p0: Bundle?) {
-                val data: ArrayList<String> =
-                    p0?.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION) as ArrayList<String>
-                textView.text = data[0]
-            }
-
-            override fun onPartialResults(p0: Bundle?) {
-
-            }
-
-            override fun onEvent(p0: Int, p1: Bundle?) {
-
-            }
-        })
         val button = findViewById<Button>(R.id.speakButton)
         button.setOnClickListener {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
@@ -101,14 +51,95 @@ class TestVoiceRecognitionActivity : AppCompatActivity() {
                         Manifest.permission.RECORD_AUDIO
                     ) == PackageManager.PERMISSION_GRANTED
                 ) {
-                    speechRecognizer.startListening(speechRecognizerIntent)
+                    speechRecognizerEngine.start(callback)
                 } else {
-                    requestPermissionLauncher.launch(
+                    registerForActivityResult(
+                        ActivityResultContracts.RequestPermission()
+                    ) { isGranted: Boolean ->
+                        if (isGranted) {
+                            speechRecognizerEngine.start(callback)
+                        } else {
+                            textView.text = "Зря"
+                        }
+                    }.launch(
                         Manifest.permission.RECORD_AUDIO
                     )
                 }
+            } else {
+                speechRecognizerEngine.start(callback)
             }
         }
     }
+
+}
+
+interface SpeechRecognizerCallback {
+    fun started()
+    fun finished(result: String)
+    fun error(code: Int)//todo смаппить в домен
+}
+
+interface SpeechRecognizerEngine {
+    fun start(callback: SpeechRecognizerCallback)
+    fun stop()
+
+    class Base(context: Context) : SpeechRecognizerEngine {
+
+        private val speechRecognizer = SpeechRecognizer.createSpeechRecognizer(context)
+
+        private val speechRecognizerIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+            .putExtra(
+                RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+            )
+            .putExtra(
+                RecognizerIntent.EXTRA_LANGUAGE,
+                Locale.ENGLISH.language
+            )
+
+        override fun start(callback: SpeechRecognizerCallback) {
+            speechRecognizer.setRecognitionListener(object : SimpleRecognitionListener() {
+                override fun onReadyForSpeech(p0: Bundle?) {
+                    callback.started()
+                }
+
+                override fun onError(p0: Int) {
+                    callback.error(p0)
+                }
+
+                override fun onResults(p0: Bundle?) {
+                    val data: ArrayList<String> =
+                        p0?.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION) as ArrayList<String>
+                    callback.finished(data[0])
+                }
+            })
+
+            speechRecognizer.startListening(speechRecognizerIntent)
+        }
+
+        override fun stop() {
+            speechRecognizer.stopListening()
+        }
+    }
+}
+
+abstract class SimpleRecognitionListener : RecognitionListener {
+    override fun onReadyForSpeech(params: Bundle?) = Unit
+
+    override fun onBeginningOfSpeech() = Unit
+
+    override fun onRmsChanged(rmsdB: Float) = Unit
+
+    override fun onBufferReceived(buffer: ByteArray?) = Unit
+
+    override fun onEndOfSpeech() = Unit
+
+    override fun onError(error: Int) = Unit
+
+    override fun onResults(results: Bundle?) = Unit
+
+    override fun onPartialResults(partialResults: Bundle?) = Unit
+
+    override fun onEvent(eventType: Int, params: Bundle?) = Unit
 
 }
