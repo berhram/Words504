26b519f24416105bf3ddd0af1b725f7bed5fbae0
Roman Troyan <troyan.roman1985@gmail.com>
2023-04-06 20:26:45 +0300

    ADMT-0002 Custom input layout constructor corrected

--- app/src/main/java/ru/easycode/words504/admintools/WordsIndexesInputLayout.kt
+++ app/src/main/java/ru/easycode/words504/admintools/WordsIndexesInputLayout.kt
@@ -3,43 +3,32 @@ package ru.easycode.words504.admintools
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
-import com.google.android.material.R.styleable.TextInputLayout
 import com.google.android.material.textfield.TextInputLayout
 import ru.easycode.words504.R
 import ru.easycode.words504.admintools.input.ApostropheMatch
 import ru.easycode.words504.admintools.input.HandleIndexes
-import ru.easycode.words504.admintools.input.InitView
 import ru.easycode.words504.admintools.input.LetterMatch
 import ru.easycode.words504.admintools.input.WordsIndexesTextWatcher
 import ru.easycode.words504.domain.Mapper
 
-class WordsIndexesInputLayout : TextInputLayout, Mapper.Unit<String>, InitView {
+class WordsIndexesInputLayout : TextInputLayout, Mapper.Unit<String> {
     constructor(context: Context) : super(context)
-    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
-        init(attrs)
-    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
 
     constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(
         context,
         attrs,
         defStyleAttr
-    ) {
-        init(attrs)
-    }
+    )
 
     private val handle = HandleIndexes(LetterMatch(), ApostropheMatch())
     private val simpleWatcher = WordsIndexesTextWatcher(this, handle)
 
-    override fun init(attrs: AttributeSet) {
+    init {
         (context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater)
             .inflate(R.layout.words_indexes_input_layout, this, true)
-        context.theme.obtainStyledAttributes(attrs, TextInputLayout, 0, 0).apply {
-            try {
-                editText?.addTextChangedListener(simpleWatcher)
-            } finally {
-                recycle()
-            }
-        }
+        editText?.addTextChangedListener(simpleWatcher)
     }
 
     override fun map(source: String) {
--- app/src/main/java/ru/easycode/words504/admintools/input/InitView.kt
+++
@@ -1,7 +0,0 @@
-package ru.easycode.words504.admintools.input
-
-import android.util.AttributeSet
-
-interface InitView {
-    fun init(attrs: AttributeSet)
-}
