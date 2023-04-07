e2b2301a5e2ad6f681c26a37feb9285db8c2313d
TroyanRoman <61114665+TroyanRoman@users.noreply.github.com>
2023-04-07 14:33:35 +0300

    Admt 0002 custom input layout (#61)

---
+++ app/src/main/java/ru/easycode/words504/admintools/input/HandleIndexes.kt
@@ -0,0 +1,25 @@
+package ru.easycode.words504.admintools.input
+
+import ru.easycode.words504.domain.Mapper
+
+class HandleIndexes(
+    private val letter: Mapper<Char, Boolean>,
+    private val apostrophe: Mapper<Char, Boolean>
+) : Mapper<String, String> {
+    override fun map(source: String): String {
+        var output = ""
+        source.forEachIndexed { index, char ->
+            val previousSymbol = if (index > 0) source[index - 1] else SPACE_CHAR
+            val showIndex = with(letter) {
+                map(previousSymbol).not() && map(char) && apostrophe.map(previousSymbol).not()
+            }
+            output += if (showIndex) index else FILLER_CHAR
+        }
+        return output
+    }
+
+    companion object {
+        private const val SPACE_CHAR = ' '
+        private const val FILLER_CHAR = '_'
+    }
+}
