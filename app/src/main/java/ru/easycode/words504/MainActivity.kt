590e310d77fc4adbf4988e4b5cfbf12d4c9401be
Roman Troyan <troyan.roman1985@gmail.com>
2023-04-08 22:32:03 +0300

    CORE-0022 implemented ProvideViewModel & BaseFragment created

--- app/src/main/java/ru/easycode/words504/MainActivity.kt
+++ app/src/main/java/ru/easycode/words504/MainActivity.kt
@@ -2,10 +2,16 @@ package ru.easycode.words504
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelStoreOwner
+import ru.easycode.words504.sl.ProvideViewModel
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ProvideViewModel {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun <T : ViewModel> viewModel(clazz: Class<T>, owner: ViewModelStoreOwner): T =
+        (application as ProvideViewModel).viewModel(clazz, owner)
 }
