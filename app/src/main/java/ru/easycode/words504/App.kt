d4c02d2c7794a93183ae8b9cb22ff3c240b226cd
ekzakh <122279213+ekzakh@users.noreply.github.com>
2023-04-10 13:08:23 +0300

    Core-0027 view model factory in app (#77)

--- app/src/main/java/ru/easycode/words504/App.kt
+++ app/src/main/java/ru/easycode/words504/App.kt
@@ -2,12 +2,29 @@ package ru.easycode.words504
 
 import android.app.Application
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelStoreOwner
+import ru.easycode.words504.sl.BaseDependencyContainer
+import ru.easycode.words504.sl.CoreModule
 import ru.easycode.words504.sl.ProvideViewModel
+import ru.easycode.words504.sl.ViewModelsFactory
 
 class App : Application(), ProvideViewModel {
 
-    override fun <T : ViewModel> viewModel(clazz: Class<T>, owner: ViewModelStoreOwner): T {
-        TODO()
+    private lateinit var viewModelsFactory: ViewModelsFactory
+
+    override fun onCreate() {
+        super.onCreate()
+        viewModelsFactory = ViewModelsFactory(
+            BaseDependencyContainer(
+                CoreModule.Base(
+                    this,
+                    BuildConfig.DEBUG
+                )
+            )
+        )
     }
+
+    override fun <T : ViewModel> viewModel(clazz: Class<T>, owner: ViewModelStoreOwner): T =
+        ViewModelProvider(owner, viewModelsFactory)[clazz]
 }
--- core/src/main/java/ru/easycode/words504/sl/ViewModelsFactory.kt
+++ core/src/main/java/ru/easycode/words504/sl/ViewModelsFactory.kt
@@ -8,5 +8,5 @@ class ViewModelsFactory(
 ) : ViewModelProvider.Factory {
 
     override fun <T : ViewModel> create(modelClass: Class<T>): T =
-        dependencyContainer.module(modelClass) as T
+        dependencyContainer.module(modelClass).viewModel() as T
 }
