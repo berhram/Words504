8a87a1da48f802cc9219677f987435e1bed379c7
EkZakh <ekzakh14@gmail.com>
2023-04-11 20:56:35 +0300

    fix lint

--- app/src/main/java/ru/easycode/words504/admintools/data/cloud/ExerciseType.kt
+++ app/src/main/java/ru/easycode/words504/admintools/data/cloud/ExerciseType.kt
@@ -5,18 +5,25 @@ import com.google.gson.annotations.SerializedName
 enum class ExerciseType {
     @SerializedName("fillBlank")
     FILL_BLANK,
+
     @SerializedName("trueFalse")
     TRUE_FALSE,
+
     @SerializedName("opposites")
     OPPOSITES,
+
     @SerializedName("synonym")
     SYNONYM,
+
     @SerializedName("wordDetective")
     WORD_DETECTIVE,
+
     @SerializedName("choseCorrect")
     CHOSE_CORRECT,
+
     @SerializedName("matching")
     MATCHING,
+
     @SerializedName("letterOfCompliant")
     LETTER_OF_COMPLIANT
 }
