4640437ced4ba7a440f777e4c95e622fa1e10808
EkZakh <ekzakh14@gmail.com>
2023-04-13 10:48:43 +0300

    navigation strategy

--- core/src/main/java/ru/easycode/words504/presentation/Screen.kt
+++ core/src/main/java/ru/easycode/words504/presentation/Screen.kt
@@ -1,17 +1,13 @@
 package ru.easycode.words504.presentation
 
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 
 interface Screen {
     fun replace(manager: FragmentManager)
 
-    abstract class Base(
-        private val clazz: Class<out Fragment>,
-        private val resId: Int = com.google.android.material.R.id.container
-    ) : Screen {
+    abstract class Base(private val navigationStrategy: NavigationStrategy) : Screen {
         override fun replace(manager: FragmentManager) {
-            manager.beginTransaction().replace(resId, clazz.newInstance()).commit()
+            navigationStrategy.navigate(manager)
         }
     }
 }
