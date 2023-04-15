ab9979e9d709186bb2fdd5e0a01fccc086d3f109
Sabina <43931110+eels911@users.noreply.github.com>
2023-04-15 18:01:26 +0300

    fix/TRNS-0003_fix_Translate_Service (#82)

--- app/src/main/java/ru/easycode/words504/translate/data/cloud/TranslateService.kt
+++ app/src/main/java/ru/easycode/words504/translate/data/cloud/TranslateService.kt
@@ -1,4 +1,4 @@
-package ru.easycode.words504.languages.data.cloud
+package ru.easycode.words504.translate.data.cloud
 
 import retrofit2.Call
 import retrofit2.http.POST
@@ -9,6 +9,7 @@ interface TranslateService {
     @POST("translate")
     fun translate(
         @Query("target_lang") targetLang: String,
-        @Query("text") text: String
+        @Query("text") text: String,
+        @Query("source_lang") sourceLang: String = "EN"
     ): Call<TranslateCloud.Base>
 }
