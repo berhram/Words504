c40b3eb2e5e683b5c9ca72d1220147bc7245189b
hadron <hadron@yandex.ru>
2023-04-18 08:26:24 +0300

    ADMT-0005 added Admintools activity

--- app/src/main/java/ru/easycode/words504/MainActivity.kt
+++ app/src/main/java/ru/easycode/words504/MainActivity.kt
@@ -1,14 +1,23 @@
 package ru.easycode.words504
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import ru.easycode.words504.admintools.presentation.AdminActivity
 import ru.easycode.words504.presentation.BaseActivity
 
 class MainActivity : BaseActivity<MainViewModel>() {
 
     override val viewModelClass: Class<MainViewModel> = MainViewModel::class.java
+    private lateinit var gotoAdminButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        gotoAdminButton = findViewById(R.id.gotoAdminButton)
+        gotoAdminButton.setOnClickListener {
+            startActivity(Intent(this, AdminActivity::class.java))
+        }
     }
 }
---
+++ app/src/main/java/ru/easycode/words504/admintools/presentation/AdminActivity.kt
@@ -0,0 +1,14 @@
+package ru.easycode.words504.admintools.presentation
+
+import android.os.Bundle
+import ru.easycode.words504.R
+import ru.easycode.words504.presentation.BaseActivity
+
+class AdminActivity : BaseActivity<AdminViewModel>() {
+    override val viewModelClass: Class<AdminViewModel> = AdminViewModel::class.java
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_admin)
+    }
+}
