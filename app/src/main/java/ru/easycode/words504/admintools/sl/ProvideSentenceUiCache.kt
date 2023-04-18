aad33c7bc7dde6239757d1c01e1244f57020ca80
EkZakh <ekzakh14@gmail.com>
2023-04-18 13:38:30 +0300

    admt-0011 sentence cache

--- app/src/main/java/ru/easycode/words504/admintools/sl/ProvideSentenceUiCache.kt
+++ app/src/main/java/ru/easycode/words504/admintools/sl/ProvideSentenceUiCache.kt
@@ -3,5 +3,5 @@ package ru.easycode.words504.admintools.sl
 import ru.easycode.words504.admintools.presentation.SentenceUiCache
 
 interface ProvideSentenceUiCache {
-    fun provideSentenceUiCache(): SentenceUiCache
+    fun provideSentenceUiCache(): SentenceUiCache.Mutable
 }
