16a11a59a5c523d087163c99bef889ac45667d29
ekzakh <122279213+ekzakh@users.noreply.github.com>
2023-04-18 08:57:54 +0300

    ADMT-0010 sentence ui model (#94)
    admt-0010 sentence ui

---
+++ app/src/main/java/ru/easycode/words504/admintools/presentation/WordUi.kt
@@ -0,0 +1,18 @@
+package ru.easycode.words504.admintools.presentation
+
+interface WordUi {
+
+    interface Mapper<T> {
+        fun map(ui: String, index: Int, dictionaryForm: String): T
+    }
+
+    fun <T> map(mapper: Mapper<T>): T
+
+    data class Base(
+        private val ui: String,
+        private val index: Int,
+        private val dictionaryForm: String
+    ) : WordUi {
+        override fun <T> map(mapper: Mapper<T>): T = mapper.map(ui, index, dictionaryForm)
+    }
+}
