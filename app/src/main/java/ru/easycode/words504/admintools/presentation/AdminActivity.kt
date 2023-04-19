925ee164a5551666610b8b588c1ec58bb575d294
KatyaPopova <hadron@yandex.ru>
2023-04-19 03:09:13 +0300

    ADMT-0005 fix Admintools activity

--- app/src/main/java/ru/easycode/words504/admintools/presentation/AdminActivity.kt
+++ app/src/main/java/ru/easycode/words504/admintools/presentation/AdminActivity.kt
@@ -6,9 +6,9 @@ import ru.easycode.words504.presentation.BaseActivity
 
 class AdminActivity : BaseActivity<AdminViewModel>() {
     override val viewModelClass: Class<AdminViewModel> = AdminViewModel::class.java
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin)
+        viewModel.observe(this) {}
     }
 }
