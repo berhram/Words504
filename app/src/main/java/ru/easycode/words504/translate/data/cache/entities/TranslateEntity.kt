7d66d974a995e8d58455a70c1d89a2d86fc59d82
Sabina <you@example.com>
2023-04-19 19:09:26 +0300

    TRNS-0004 fix ktlint

--- app/src/main/java/ru/easycode/words504/translate/data/cache/entities/TranslateEntity.kt
+++ app/src/main/java/ru/easycode/words504/translate/data/cache/entities/TranslateEntity.kt
@@ -2,12 +2,11 @@ package ru.easycode.words504.translate.data.cache.entities
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import java.io.Serializable
 
 @Entity(tableName = "translate_table")
 data class TranslateEntity(
     @PrimaryKey
-    val id: String,
-    val text: String,
-    val targetLang: String
-) : Serializable
+    private val id: String,
+    private val text: String,
+    private val targetLang: String
+)
