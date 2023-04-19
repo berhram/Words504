edc534e0972a4f8292e812dd7bce93f69229d9d7
Roman Troyan <troyan.roman1985@gmail.com>
2023-04-19 15:36:09 +0300

    Rsrc 0006 Coroutines research lint fixed

--- app/src/main/java/ru/easycode/words504/loading/LoadTranslateActivity.kt
+++ app/src/main/java/ru/easycode/words504/loading/LoadTranslateActivity.kt
@@ -2,8 +2,8 @@ package ru.easycode.words504.loading
 
 import android.os.Bundle
 import ru.easycode.words504.databinding.ActivityLoadCoroutinesBinding
-import ru.easycode.words504.presentation.BaseActivity
 import ru.easycode.words504.loading.adapter.LoadTranslationsAdapter
+import ru.easycode.words504.presentation.BaseActivity
 
 class LoadTranslateActivity : BaseActivity<LoadTranslateViewModel>() {
     override val viewModelClass: Class<LoadTranslateViewModel> = LoadTranslateViewModel::class.java
--- app/src/main/java/ru/easycode/words504/loading/LoadTranslateCommunication.kt
+++ app/src/main/java/ru/easycode/words504/loading/LoadTranslateCommunication.kt
@@ -1,7 +1,7 @@
 package ru.easycode.words504.loading
 
 import androidx.lifecycle.MutableLiveData
-import ru.easycode.words504.presentation.Communication
 import ru.easycode.words504.loading.adapter.TranslateResultUi
+import ru.easycode.words504.presentation.Communication
 
 class LoadTranslateCommunication : Communication.Abstract<TranslateResultUi>(MutableLiveData())
