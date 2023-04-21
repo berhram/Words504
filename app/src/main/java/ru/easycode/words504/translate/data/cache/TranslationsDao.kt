c60e791d1c5a02a624c392bc053e787761b30082
Sabina <you@example.com>
2023-04-21 20:45:21 +0300

    TRNS-0005 TranslationsDao

---
+++ app/src/main/java/ru/easycode/words504/translate/data/cache/TranslationsDao.kt
@@ -0,0 +1,23 @@
+package ru.easycode.words504.translate.data.cache
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import ru.easycode.words504.translate.data.cache.entities.TranslationCache
+
+@Dao
+interface TranslationsDao {
+
+    @Query("SELECT EXISTS(SELECT * FROM translations_table WHERE id = :id AND targetLang = :targetLang)")
+    fun isExistTranslationByIdAndLang(id: String,targetLang: String): Boolean
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(tr: TranslationCache)
+
+    @Query("DELETE FROM translations_table WHERE targetLang = :targetLang")
+    fun deleteTranslation(targetLang: String)
+
+    @Query("SELECT * FROM translations_table WHERE id = :id")
+    fun getTranslationById(id: String): TranslationCache
+}
