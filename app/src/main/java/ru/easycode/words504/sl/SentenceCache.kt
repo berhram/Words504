22439b194c4bbac36419b6879977d2099037b6f5
Sabina <you@example.com>
2023-04-23 11:52:19 +0300

    DICT-0009 sentence cache

---
+++ app/src/main/java/ru/easycode/words504/sl/SentenceCache.kt
@@ -0,0 +1,14 @@
+package ru.easycode.words504.sl
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "sentence_table")
+data class SentenceCache(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    val id: Int,
+    @ColumnInfo(name = "sentence")
+    val sentence: String
+)
