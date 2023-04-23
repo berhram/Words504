2ba5b10d0700fd9dc2cb198e08d56bb44a88a9b9
EkZakh <ekzakh14@gmail.com>
2023-04-23 07:03:46 +0300

    fix lint

--- app/src/main/java/ru/easycode/words504/sl/BaseDependencyContainer.kt
+++ app/src/main/java/ru/easycode/words504/sl/BaseDependencyContainer.kt
@@ -2,10 +2,10 @@ package ru.easycode.words504.sl
 
 import androidx.lifecycle.ViewModel
 import ru.easycode.words504.MainViewModel
-import ru.easycode.words504.loading.LoadCoroutinesModule
-import ru.easycode.words504.loading.LoadTranslateViewModel
 import ru.easycode.words504.languages.presentation.ChooseLanguageViewModel
 import ru.easycode.words504.languages.sl.ChooseLanguageModule
+import ru.easycode.words504.loading.LoadCoroutinesModule
+import ru.easycode.words504.loading.LoadTranslateViewModel
 
 class BaseDependencyContainer(
     private val core: CoreModule,
