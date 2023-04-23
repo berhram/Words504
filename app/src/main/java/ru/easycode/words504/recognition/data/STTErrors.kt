051d4189fa716c71ba8acbe4adc0d2a9e19a03fc
EkZakh <ekzakh14@gmail.com>
2023-04-23 22:47:12 +0300

    VRG-0001 fix magic numbers

--- app/src/main/java/ru/easycode/words504/recognition/data/STTErrors.kt
+++ app/src/main/java/ru/easycode/words504/recognition/data/STTErrors.kt
@@ -9,7 +9,7 @@ interface STTError {
     fun string(manageResources: ManageResources): String
 
     abstract class Abstract(
-        @StringRes protected open val errorMessage: Int
+        @StringRes protected open val errorMessage: Int,
     ) : STTError {
         override fun string(manageResources: ManageResources) = manageResources.string(errorMessage)
     }
@@ -36,22 +36,40 @@ interface STTErrorsFactory {
 
     class Base : STTErrorsFactory {
         override fun createError(id: Int): STTError = when (id) {
-            0 -> STTError.ErrorNone()
-            1 -> STTError.ErrorNetworkTimeout()
-            2 -> STTError.ErrorNetwork()
-            3 -> STTError.ErrorAudio()
-            4 -> STTError.ErrorServer()
-            5 -> STTError.ErrorClient()
-            6 -> STTError.ErrorSpeechTimeout()
-            7 -> STTError.ErrorNoMatch()
-            8 -> STTError.ErrorRecognizerBusy()
-            9 -> STTError.ErrorInsufficientPermissions()
-            10 -> STTError.ErrorTooManyRequests()
-            11 -> STTError.ErrorServerDisconnected()
-            12 -> STTError.ErrorLanguageNotSupported()
-            13 -> STTError.ErrorLanguageUnavailable()
-            14 -> STTError.ErrorCannotCheckSupport()
+            ERROR_NONE -> STTError.ErrorNone()
+            ERROR_NETWORK_TIMEOUT -> STTError.ErrorNetworkTimeout()
+            ERROR_NETWORK -> STTError.ErrorNetwork()
+            ERROR_AUDIO -> STTError.ErrorAudio()
+            ERROR_SERVER -> STTError.ErrorServer()
+            ERROR_CLIENT -> STTError.ErrorClient()
+            ERROR_SPEECH_TIMEOUT -> STTError.ErrorSpeechTimeout()
+            ERROR_NO_MATCH -> STTError.ErrorNoMatch()
+            ERROR_RECOGNIZER_BUSY -> STTError.ErrorRecognizerBusy()
+            ERROR_INSUFFICIENT_PERMISSIONS -> STTError.ErrorInsufficientPermissions()
+            ERROR_TOO_MANY_REQUESTS -> STTError.ErrorTooManyRequests()
+            ERROR_SERVER_DISCONNECTED -> STTError.ErrorServerDisconnected()
+            ERROR_LANGUAGE_NOT_SUPPORTED -> STTError.ErrorLanguageNotSupported()
+            ERROR_LANGUAGE_UNAVAILABLE -> STTError.ErrorLanguageUnavailable()
+            ERROR_LANGUAGE_CANNOT_CHECK_SUPPORT -> STTError.ErrorCannotCheckSupport()
             else -> throw java.lang.IllegalStateException("Unknown error source: $id")
         }
+    }
+
+    companion object {
+        private const val ERROR_NONE = 0
+        private const val ERROR_NETWORK_TIMEOUT = 1
+        private const val ERROR_NETWORK = 2
+        private const val ERROR_AUDIO = 3
+        private const val ERROR_SERVER = 4
+        private const val ERROR_CLIENT = 5
+        private const val ERROR_SPEECH_TIMEOUT = 6
+        private const val ERROR_NO_MATCH = 7
+        private const val ERROR_RECOGNIZER_BUSY = 8
+        private const val ERROR_INSUFFICIENT_PERMISSIONS = 9
+        private const val ERROR_TOO_MANY_REQUESTS = 10
+        private const val ERROR_SERVER_DISCONNECTED = 11
+        private const val ERROR_LANGUAGE_NOT_SUPPORTED = 12
+        private const val ERROR_LANGUAGE_UNAVAILABLE = 13
+        private const val ERROR_LANGUAGE_CANNOT_CHECK_SUPPORT = 14
     }
 }
