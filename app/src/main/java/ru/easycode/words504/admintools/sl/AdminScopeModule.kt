9fe88099ff5bc842a6ea1c7642217fca266f8de8
blastka <svetuk505@mail.ru>
2023-04-24 19:22:58 +0300

    feature/ADMT-0013 create objects

--- app/src/main/java/ru/easycode/words504/admintools/sl/AdminScopeModule.kt
+++ app/src/main/java/ru/easycode/words504/admintools/sl/AdminScopeModule.kt
@@ -1,17 +1,39 @@
 package ru.easycode.words504.admintools.sl
 
+import android.content.Context
 import ru.easycode.words504.admintools.presentation.SentenceUiCache
+import ru.easycode.words504.data.cache.preferences.ProvideSharedPreferences
+import ru.easycode.words504.data.cache.serialization.Serialization
+import ru.easycode.words504.data.cache.storage.ObjectStorage
+import ru.easycode.words504.data.cache.storage.SimpleStorage
 import ru.easycode.words504.presentation.NavigationCommunication
 import ru.easycode.words504.sl.ProvideNavigation
+import ru.easycode.words504.sl.ProvideObjectStorage
+import ru.easycode.words504.sl.ProvideSimpleStorage
 
-interface AdminScopeModule : ProvideNavigation, ProvideSentenceUiCache {
-    class Base : AdminScopeModule {
+interface AdminScopeModule : ProvideNavigation, ProvideSentenceUiCache, ProvideSimpleStorage, ProvideObjectStorage {
+    class Base(isDebug: Boolean, context: Context) : AdminScopeModule {
 
         private val navigationCommunication = NavigationCommunication.Base()
         private val sentenceUiCache = SentenceUiCache.Base()
+        private val sharedPref = if (isDebug) {
+            ProvideSharedPreferences.Debug(context)
+        } else {
+            ProvideSharedPreferences.Release(context)
+        }
+        private val simpleStorage = SimpleStorage.Base(sharedPref)
+
+        private val objectStorage = ObjectStorage.Base(
+            Serialization.Base(),
+            simpleStorage
+        )
 
         override fun provideNavigation() = navigationCommunication
 
         override fun provideSentenceUiCache() = sentenceUiCache
+
+        override fun provideSimpleStorage() = simpleStorage
+
+        override fun provideObjectStorage() = objectStorage
     }
 }
---
+++ app/src/main/java/ru/easycode/words504/sl/ProvideSimpleStorage.kt
@@ -0,0 +1,8 @@
+package ru.easycode.words504.sl
+
+import ru.easycode.words504.data.cache.storage.ObjectStorage
+import ru.easycode.words504.data.cache.storage.SimpleStorage
+
+interface ProvideSimpleStorage {
+    fun provideSimpleStorage(): SimpleStorage
+}
