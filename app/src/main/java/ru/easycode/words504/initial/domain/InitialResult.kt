3e9ee8c6e7e64744458fc6853fa27d0843a83e19
blastka <80570195+blastka@users.noreply.github.com>
2023-04-28 07:23:37 +0300

    Feature/init 0002 (#117)

---
+++ app/src/main/java/ru/easycode/words504/initial/domain/InitialResult.kt
@@ -0,0 +1,39 @@
+package ru.easycode.words504.initial.domain
+
+import ru.easycode.words504.MainScreen
+import ru.easycode.words504.initial.presentation.InitialCommunication
+import ru.easycode.words504.initial.presentation.InitialUiState
+import ru.easycode.words504.languages.presentation.ChooseLanguageScreen
+import ru.easycode.words504.presentation.NavigationCommunication
+
+interface InitialResult {
+
+    fun map(communication: InitialCommunication, navigation: NavigationCommunication.Update)
+
+    class NotFirstOpening : InitialResult {
+        override fun map(
+            communication: InitialCommunication,
+            navigation: NavigationCommunication.Update
+        ) {
+            navigation.map(MainScreen)
+        }
+    }
+
+    class FirstOpening : InitialResult {
+        override fun map(
+            communication: InitialCommunication,
+            navigation: NavigationCommunication.Update
+        ) {
+            navigation.map(ChooseLanguageScreen)
+        }
+    }
+
+    class Error(private val message: String) : InitialResult {
+        override fun map(
+            communication: InitialCommunication,
+            navigation: NavigationCommunication.Update
+        ) {
+            communication.map(InitialUiState.Error(message))
+        }
+    }
+}
