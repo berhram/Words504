f03b111b8ff1f25e7b6e7fc298ceb324419e9097
EkZakh <ekzakh14@gmail.com>
2023-04-29 14:15:44 +0300

    CORE-0029 fragment view binding

--- core/src/main/java/ru/easycode/words504/presentation/BaseFragment.kt
+++ core/src/main/java/ru/easycode/words504/presentation/BaseFragment.kt
@@ -1,18 +1,40 @@
 package ru.easycode.words504.presentation
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModel
+import androidx.viewbinding.ViewBinding
 import ru.easycode.words504.sl.ProvideViewModel
 
-abstract class BaseFragment<T : ViewModel> : Fragment() {
+abstract class BaseFragment<T : ViewModel, V : ViewBinding> : Fragment() {
+
+    private var _binding: V? = null
 
     protected lateinit var viewModel: T
-
     protected abstract val viewModelClass: Class<T>
+    protected val binding get() = _binding!!
+
+    abstract fun fragmentBinding(inflater: LayoutInflater, container: ViewGroup?): V
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewModel = (requireActivity() as ProvideViewModel).viewModel(viewModelClass, this)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = fragmentBinding(inflater, container)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
