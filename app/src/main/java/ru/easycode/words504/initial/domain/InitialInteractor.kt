aecf73f45973899d00515df75852ed4cd62579c6
hovhannes <johnnysc091@gmail.com>
2023-04-30 12:55:22 +0900

    feature/init-0003-initial-interactor interactor class

--- app/src/main/java/ru/easycode/words504/initial/domain/InitialInteractor.kt
+++ app/src/main/java/ru/easycode/words504/initial/domain/InitialInteractor.kt
@@ -1,5 +1,33 @@
 package ru.easycode.words504.initial.domain
 
+import ru.easycode.words504.domain.HandleError
+
 interface InitialInteractor {
+
     suspend fun init(): InitialResult
+
+    class Base(
+        private val repository: InitialRepository,
+        private val handleError: HandleError<java.lang.Exception, String>
+    ) : InitialInteractor {
+
+        override suspend fun init(): InitialResult = try {
+            if (repository.userHasChosenLanguage()) {
+                InitialResult.NotFirstOpening
+            } else {
+                repository.init()
+                InitialResult.FirstOpening
+            }
+        } catch (e: Exception) {
+            val message = handleError.handle(e)
+            InitialResult.Error(message)
+        }
+    }
+}
+
+interface InitialRepository {
+
+    fun userHasChosenLanguage(): Boolean
+
+    suspend fun init()
 }
