0aa6f18fddd570e8b91560d423d05bdd42a37161
EkZakh <ekzakh14@gmail.com>
2023-05-03 08:48:08 +0300

    initial module

---
+++ app/src/main/java/ru/easycode/words504/initial/domain/HandleUiError.kt
@@ -0,0 +1,11 @@
+package ru.easycode.words504.initial.domain
+
+import ru.easycode.words504.R
+import ru.easycode.words504.domain.HandleError
+import ru.easycode.words504.presentation.ManageResources
+
+class HandleUiError(private val manageResources: ManageResources): HandleError<Throwable, String> {
+    override fun handle(source: Throwable): String {
+        return source.message ?: manageResources.string(R.string.error)
+    }
+}
