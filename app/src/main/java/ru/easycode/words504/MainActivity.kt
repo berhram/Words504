db0bd8daad1f7c55698c0a02e58f89f76d88ad88
Sabina <you@example.com>
2023-05-03 11:11:29 +0300

    del unused

--- app/src/main/java/ru/easycode/words504/MainActivity.kt
+++ app/src/main/java/ru/easycode/words504/MainActivity.kt
@@ -3,13 +3,7 @@ package ru.easycode.words504
 import android.content.Intent
 import android.os.Bundle
 import ru.easycode.words504.admintools.presentation.AdminActivity
-import ru.easycode.words504.data.cache.preferences.ProvideSharedPreferences
-import ru.easycode.words504.data.cache.serialization.Serialization
-import ru.easycode.words504.data.cache.storage.ObjectStorage
-import ru.easycode.words504.data.cache.storage.SimpleStorage
 import ru.easycode.words504.databinding.ActivityMainBinding
-import ru.easycode.words504.languages.data.cache.LanguageCache
-import ru.easycode.words504.languages.data.cache.LanguagesCacheDataSource
 import ru.easycode.words504.languages.presentation.ChooseLanguageScreen
 import ru.easycode.words504.loading.LoadTranslateActivity
 import ru.easycode.words504.presentation.BaseActivity
@@ -24,16 +18,6 @@ class MainActivity : BaseActivity<MainViewModel>() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val list = ArrayList<LanguageCache.Base>()
-        list.add(LanguageCache.Base("ru", "Russian"))
-        list.add(LanguageCache.Base("ch", "Chinese"))
-        val base = LanguagesCacheDataSource.Base(
-            objectStorage = ObjectStorage.Base(
-                Serialization.Base(),
-                SimpleStorage.Base(ProvideSharedPreferences.Debug(applicationContext))
-            )
-        )
-        base.save(list)
         if (savedInstanceState == null) {
             ChooseLanguageScreen.navigate(supportFragmentManager, R.id.container)
         }
