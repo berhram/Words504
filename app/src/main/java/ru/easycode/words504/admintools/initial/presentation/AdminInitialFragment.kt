203d949bd490bf1915ecb10160a3421c3b7dfc49
hovhannes <johnnysc091@gmail.com>
2023-05-22 17:00:55 +0900

    feature/ADMT-0015 choose custom view group

--- app/src/main/java/ru/easycode/words504/admintools/initial/presentation/AdminInitialFragment.kt
+++ app/src/main/java/ru/easycode/words504/admintools/initial/presentation/AdminInitialFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import ru.easycode.words504.admintools.core.presentation.Choose
 import ru.easycode.words504.admintools.presentation.AdminSentenceScreen
 import ru.easycode.words504.databinding.FragmentAdminInitialBinding
 import ru.easycode.words504.presentation.BaseFragment
@@ -24,6 +26,22 @@ class AdminInitialFragment : BaseFragment<AdminInitialViewModel, FragmentAdminIn
         super.onViewCreated(view, savedInstanceState)
         binding.sentenceButton.setOnClickListener {
             viewModel.navigate(AdminSentenceScreen)
+        }
+        binding.customViewGroup.init(
+            listOf(
+                Choose.Base("1", "type1"),
+                Choose.Base("2", "type2"),
+                Choose.Base("3", "type3"),
+                Choose.Base("4", "type4"),
+                Choose.Base("5", "type5"),
+                Choose.Base("6", "type6"),
+                Choose.Base("7", "type7"),
+                Choose.Base("8", "type8"),
+                Choose.Base("9", "type9"),
+                Choose.Base("10", "type10"),
+            )
+        ) {
+            Toast.makeText(requireContext(), "Chosen $it", Toast.LENGTH_LONG).show()
         }
     }
 }
