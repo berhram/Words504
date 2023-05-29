9998430ed3ba8c85bab1465cdd1a2cb0c3046fbf
EkZakh <ekzakh14@gmail.com>
2023-05-29 15:42:10 +0300

    admt-0017 rename mapper

--- app/src/main/java/ru/easycode/words504/admintools/lessonslist/data/cache/LessonCacheMapper.kt
+++ app/src/main/java/ru/easycode/words504/admintools/lessonslist/data/cache/LessonCacheMapper.kt
@@ -2,7 +2,7 @@ package ru.easycode.words504.admintools.lessonslist.data.cache
 
 import ru.easycode.words504.admintools.lessonslist.presentation.LessonUi
 
-class LessonUiMapper : LessonCache.Mapper<LessonUi> {
+class LessonCacheMapper : LessonCache.Mapper<LessonUi> {
     override fun map(id: String, isComplete: Boolean): LessonUi =
         if (isComplete) LessonUi.Complete(id) else LessonUi.InProgress(id)
 }
