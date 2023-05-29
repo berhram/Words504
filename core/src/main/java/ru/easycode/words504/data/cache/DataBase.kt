c5894e6f11c64068c166cc722e5a7d2d2442f959
EkZakh <ekzakh14@gmail.com>
2023-05-29 15:36:07 +0300

    admt-0017 fix lint

--- core/src/main/java/ru/easycode/words504/data/cache/DataBase.kt
+++ core/src/main/java/ru/easycode/words504/data/cache/DataBase.kt
@@ -1,6 +1,6 @@
 package ru.easycode.words504.data.cache
 
-interface DataBase<T: Any> {
-    fun  create(): T
+interface DataBase<T : Any> {
+    fun create(): T
     fun <K : Any> create(typeConverter: K): T
 }
