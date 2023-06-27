363fca04e8404f55dd1c894a63144cc04d50fe76
velvet <berhram@protonmail.com>
2023-06-27 13:27:06 +0300

    tts-0001-c0reTTS

--- app/src/main/java/ru/easycode/words504/tts/data/TTSControlObserver.kt
+++ app/src/main/java/ru/easycode/words504/tts/data/TTSControlObserver.kt
@@ -1,7 +1,7 @@
 package ru.easycode.words504.tts.data
 
 interface TTSControlObserver {
-    // TODO должны ли эти методы принмать какие -то значения. Например пауза на каком слове, восстановиль с какого
+
     fun paused()
 
     fun resumed()
--- app/src/main/java/ru/easycode/words504/tts/data/TTSEngine.kt
+++ app/src/main/java/ru/easycode/words504/tts/data/TTSEngine.kt
@@ -4,9 +4,7 @@ import android.content.Context
 import android.speech.tts.TextToSpeech
 import android.speech.tts.UtteranceProgressListener
 import ru.easycode.words504.tts.domain.TTSErrorsFactory
-import java.util.LinkedList
 import java.util.Locale
-import java.util.Queue
 
 interface TTSEngine : TTSControl {
 
@@ -26,7 +24,7 @@ interface TTSEngine : TTSControl {
 
         private lateinit var tts: TextToSpeech
 
-        private val wordsQueue: Queue<String> = LinkedList()
+        private val wordsQueue: MutableList<String> = ArrayList()
         private var isPaused: Boolean = false
 
         override fun addObserver(observer: TTSObserver) {
@@ -40,31 +38,25 @@ interface TTSEngine : TTSControl {
                 override fun onStop(utteranceId: String?, interrupted: Boolean) {
                     super.onStop(utteranceId, interrupted)
                     if (interrupted && !isPaused) {
-                        wordsQueue.offer(utteranceId)
-                        // TODO надо расставить в правильных местах колбэки TTSControlObserver для панельки управления
                         controlObserversStorage.notify { it.stopped() }
                     } else {
-                        // TODO надо расставить в правильных местах колбэки TTSControlObserver для панельки управления
                         controlObserversStorage.notify { it.paused() }
                     }
                 }
 
                 override fun onStart(utteranceId: String) {
                     observersStorage.notify { it.started(utteranceId) }
-                    // TODO надо расставить в правильных местах колбэки TTSControlObserver для панельки управления
                     controlObserversStorage.notify { it.resumed() }
                 }
 
                 override fun onDone(utteranceId: String) {
+                    wordsQueue.removeLast()
                     observersStorage.notify { it.finished(utteranceId) }
                     if (!isPaused) {
-                        wordsQueue.poll()?.let {
-                            ttsSpeak(it)
-                        }
+                        ttsSpeak(wordsQueue.last())
                     }
                 }
 
-                // TODO не получилось эмулировать ошибку, у меня и без интернета читает
                 @Deprecated("Deprecated in Java", ReplaceWith("Unit"))
                 override fun onError(utteranceId: String) = Unit
 
@@ -81,9 +73,7 @@ interface TTSEngine : TTSControl {
             tts.stop()
             wordsQueue.clear()
             wordsQueue.addAll(phrases)
-            wordsQueue.poll()?.let {
-                ttsSpeak(it)
-            }
+            ttsSpeak(wordsQueue.last())
         }
 
         override fun pause() {
@@ -93,13 +83,11 @@ interface TTSEngine : TTSControl {
             }
         }
 
-        // TODO Если во время чтения предложения поставить паузу, а потом возобновить, то читать начинает со следующего предложения
+
         override fun resume() {
             if (isPaused) {
                 isPaused = false
-                wordsQueue.poll()?.let {
-                    tts.speak(it, TextToSpeech.QUEUE_FLUSH, null, it)
-                }
+                tts.speak(wordsQueue.last(), TextToSpeech.QUEUE_FLUSH, null, wordsQueue.last())
             }
         }
 
